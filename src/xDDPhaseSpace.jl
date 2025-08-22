14b65b94fcbe283ff042430db436ff6cc88997e3
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-08-22 18:32:59 +0200

    polished

--- src/xDDPhaseSpace.jl
+++ src/xDDPhaseSpace.jl
@@ -3,41 +3,37 @@ module xDDPhaseSpace
 using Cuba
 using Parameters
 
-# Reexport all methods from submodules
-export AbstractxDD
-export mapdalitzmethod, masses
-export integrand_mapped_thr, ρ_thr
-include("abstractxdd.jl")
+import Base.^
+^(ms::NamedTuple{(:m1, :m2, :m3), T} where {T}, n::Int) = Tuple(ms) .^ n  # type piracy -- not good
 
-
-export e2m
 export WithThrE
+export e2m
 include("withthr.jl")
 
-
-import Base.^
-^(ms::NamedTuple{(:m1, :m2, :m3), T} where {T}, n::Int) = Tuple(ms) .^ n
-
-export σ2of3_pm, σ3of1_pm, σ3of2_pm
-export σ3of1, σ2of1
-export AbstractDalitzMapping
-export LinearDalitzMapping, HookSqrtDalitzMapping
-export mapdalitz
-include("integrationpaths.jl")
+export AbstractxDD
+export mapdalitzmethod
+export integrand_mapped_thr
+export ρ_thr, ρ_tb
+export masses
+include("abstractxdd.jl")
 
 export BW, BW_norm, BW_Swave, ZeroBW
 export Jᴵ, Jᴵᴵ
 include("isobar.jl")
 
-export πDD
+export AbstractDalitzMapping
+export HookSqrtDalitzMapping
+export LinearDalitzMapping
+export mapdalitz
+include("integrationpaths.jl")
+
 export branch_points
-export constructchannel
+export πDD
 include("covariant_exptessions.jl")
 include("mainmodel-pi.jl")
 
 export obj2nt
+export constructchannel
 include("deserialization.jl")
-
-
 
 end # xDDPhaseSpace
