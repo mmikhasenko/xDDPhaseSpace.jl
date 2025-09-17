831145956171686eaf8eba8d10a69ddfa6bf527a
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-09-17 21:06:25 +0200

    fix decerialization

--- src/deserialization.jl
+++ src/deserialization.jl
@@ -1,22 +1,24 @@
 obj2nt(nt::NamedTuple) = nt
 
 # deserilization
-function constructchannel(ser::NamedTuple{(:R12, :R13, :ms, :type),T} where {T})
-    # Try to find the type in the parent module first, then in current module
-    if startswith(ser.type, "γDD")
-        type = isdefined(Main, :X2DDpi) ? Main.X2DDpi.γDD : γDD
-    elseif startswith(ser.type, "DˣD")
-        type = isdefined(Main, :X2DDpi) ? Main.X2DDpi.DˣD : DˣD
-    else
-        type = eval(Meta.parse(ser.type))
-    end
+function constructchannel(ser::NamedTuple{(:R12, :R13, :ms, :type), T} where {T})
+    @assert startswith(ser.type, "πDD")
+    type = eval(Meta.parse(ser.type))
     ms = NamedTuple{(:m1, :m2, :m3)}(ser.ms)
     R12 = constructlineshape(ser.R12)
     R13 = constructlineshape(ser.R13)
     type(ms, R12, R13)
 end
 
-constructlineshape(R::NamedTuple{(:m, :Γ),T} where {T}) = R
+function constructchannel(ser::NamedTuple{(:R12, :R13, :μ12, :μ13, :ms, :type), T} where {T})
+    @assert startswith(ser.type, "γDD")
+    type = eval(Meta.parse(ser.type))
+    ms = NamedTuple{(:m1, :m2, :m3)}(ser.ms)
+    R12 = constructlineshape(ser.R12)
+    R13 = constructlineshape(ser.R13)
+    type(ms, R12, R13, ser.μ12, ser.μ13)
+end
+constructlineshape(R::NamedTuple{(:m, :Γ), T} where {T}) = R
 # 
 function constructlineshape(R::NamedTuple)
     type = eval(Meta.parse(R.type))
